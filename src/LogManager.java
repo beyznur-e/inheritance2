52a9d5a627fd98728e174e5efba48f345e1f498b
Beyzanur Ertem <ertembeyzanur1406@gmail.com>
2024-09-07 00:12:11 +0300

    Initial commit

---
+++ src/LogManager.java
@@ -0,0 +1,6 @@
+public class LogManager {
+    //Birbirinin alternatifi olan kod blokları "if" ile yönetilmez!! Ayrı sınıflar oluşturmak gereklidir.
+    public void log(){
+
+    }
+}
---
+++ src/Logger.java
@@ -0,0 +1,6 @@
+public class Logger {
+    //base sınıfı olsun.
+    public void log(){
+        System.out.println("Ortak konfigurasyon");
+    }
+}
